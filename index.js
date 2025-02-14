62b680b4beb8f2149b3397d5f420df5ecc5e84ec
JazxCode <prstraja5@gmail.com>
2025-02-14 14:39:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const swaggerDocument = require('./public/swagger.json');
 // Middleware untuk dokumentasi API
 app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-
 const cors = require('cors');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
